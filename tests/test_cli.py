b727fc75c55a3f2199580afff1d2922b578adc52
Jannik Luhn <jannik@brainbot.com>
2019-03-26 16:01:34 +0100

    Load and validate CSV input

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,14 +1,17 @@
 import pytest
 from click.testing import CliRunner
 
-from merkle_drop.load_json import load_airdrop_dict
+from eth_utils import to_checksum_address, to_normalized_address
+
+from merkle_drop.load_csv import load_airdrop_dict, validate_address_value_pairs
 from merkle_drop.cli import main
 
 
-airdrop_list = """{
-                    "address1": 10,
-                    "address2": 20
-               }"""
+A_ADDRESS = b"\xaa" * 20
+B_ADDRESS = b"\xbb" * 20
+
+
+AIRDROP_DICT = {A_ADDRESS: 10, B_ADDRESS: 20}
 
 
 @pytest.fixture()
@@ -16,7 +19,14 @@ def airdrop_list_file(tmp_path):
     folder = tmp_path / "subfolder"
     folder.mkdir()
     file_path = folder / "airdrop_list.json"
-    file_path.write_text(airdrop_list)
+    file_path.write_text(
+        "\n".join(
+            (
+                ",".join((to_checksum_address(address), str(value)))
+                for address, value in AIRDROP_DICT.items()
+            )
+        )
+    )
     return file_path
 
 
@@ -34,4 +44,37 @@ def test_merkle_root_cli(runner, airdrop_list_file):
 def test_read_json_file(airdrop_list_file):
 
     data = load_airdrop_dict(airdrop_list_file)
-    assert data == {"address1": 10, "address2": 20}
+    assert data == AIRDROP_DICT
+
+
+@pytest.mark.parametrize(
+    "address_value_pairs",
+    (
+        (),
+        ((to_checksum_address(A_ADDRESS), "0"),),
+        ((to_checksum_address(A_ADDRESS), "0"), (to_checksum_address(B_ADDRESS), "0")),
+        ((to_checksum_address(A_ADDRESS), str(2 ** 256 - 1)),),
+    ),
+)
+def test_valid_airdrop_file_validation(address_value_pairs):
+    validate_address_value_pairs(address_value_pairs)
+
+
+@pytest.mark.parametrize(
+    "address_value_pairs",
+    (
+        ((),),
+        ((to_checksum_address(A_ADDRESS),)),
+        ((to_checksum_address(A_ADDRESS), "0", "a third entry"),),
+        ((to_checksum_address(A_ADDRESS), "0"), (to_checksum_address(A_ADDRESS), "0")),
+        ((to_checksum_address(A_ADDRESS), "0xaa"),),
+        ((to_checksum_address(A_ADDRESS), "-3"),),
+        ((to_checksum_address(A_ADDRESS), "1.2"),),
+        ((A_ADDRESS, "0"),),
+        ((to_normalized_address(A_ADDRESS), "0"),),
+        (("0", to_checksum_address(A_ADDRESS)),),
+    ),
+)
+def test_invalid_airdrop_file_validation(address_value_pairs):
+    with pytest.raises(ValueError):
+        validate_address_value_pairs(address_value_pairs)
