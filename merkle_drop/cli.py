797a4bc8ce311c211d07d74d93096091d5cdff2a
Bernd Bohmeier <berndbohmeier@googlemail.com>
2019-03-28 00:57:50 +0100

    Add balance query
    Add option to ask for the balance of an address

--- merkle_drop/cli.py
+++ merkle_drop/cli.py
@@ -2,7 +2,7 @@ import click
 
 from eth_utils import encode_hex
 
-from .airdrop import to_items
+from .airdrop import to_items, get_balance
 from .load_csv import load_airdrop_file
 from .merkle_tree import compute_merkle_root
 
@@ -19,4 +19,15 @@ def root(airdrop_file_name):
     airdrop_data = load_airdrop_file(airdrop_file_name)
     merkle_root = compute_merkle_root(to_items(airdrop_data))
 
-    click.echo(f"The merkle root is: {encode_hex(merkle_root)}")
+    click.echo(f"{encode_hex(merkle_root)}")
+
+
+@main.command(short_help="Balance of address")
+@click.argument("address")
+@click.argument("airdrop_file_name")
+def balance(address, airdrop_file_name):
+
+    airdrop_data = load_airdrop_file(airdrop_file_name)
+    balance = get_balance(address, airdrop_data)
+
+    click.echo(f"{balance}")
