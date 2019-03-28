f9bef56e0902aceab61caef7d71cf64c31acf44b
Bernd Bohmeier <berndbohmeier@googlemail.com>
2019-03-28 18:14:50 +0100

    Always use canonical addresses internally

--- merkle_drop/airdrop.py
+++ merkle_drop/airdrop.py
@@ -1,6 +1,4 @@
-from typing import Dict, List, Union
-
-from eth_utils import to_canonical_address
+from typing import Dict, List
 
 from .merkle_tree import Item
 
@@ -11,5 +9,5 @@ def to_items(airdrop_data: AirdropData) -> List[Item]:
     return [Item(address, value) for address, value in airdrop_data.items()]
 
 
-def get_balance(address: Union[str, bytes], airdrop_data: AirdropData) -> int:
-    return airdrop_data.get(to_canonical_address(address), 0)
+def get_balance(address: bytes, airdrop_data: AirdropData) -> int:
+    return airdrop_data.get(address, 0)
