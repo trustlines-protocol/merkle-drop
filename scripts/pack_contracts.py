c559fc54e23bd9c0ae6683e07fe3967e086d19b5
cducrest <come.ducrest@gmail.com>
2019-06-24 11:44:16 +0200

    Add script to pack contracts in merkle_drop package for deployment add call to script in makefile

---
+++ scripts/pack_contracts.py
@@ -0,0 +1,24 @@
+"""This script takes an input contract.json and filters only the needed contracts
+Usage: python pack_contracts.py input_contracts.json output_contracts.json
+"""
+import json
+
+contracts = ["MerkleDrop"]
+
+
+def pack_contracts(input_filename, output_filename):
+    with open(input_filename) as f:
+        input_dict = json.load(f)
+
+    output_dict = {}
+    for contract in contracts:
+        output_dict[contract] = input_dict[contract]
+
+    with open(output_filename, "w") as f:
+        json.dump(output_dict, f, indent=2)
+
+
+if __name__ == "__main__":
+    import sys
+
+    pack_contracts(sys.argv[1], sys.argv[2])
