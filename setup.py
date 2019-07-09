529bd378f84b829ac3f9607eef032ce217281bcd
Ralf Schmitt <ralf@brainbot.com>
2019-07-09 09:27:53 +0200

    Add gunicorn to requirements.txt
    Also add flask to install_requires.

--- setup.py
+++ setup.py
@@ -5,7 +5,13 @@ setup(
     setup_requires=["setuptools_scm"],
     use_scm_version=True,
     packages=find_packages(),
-    install_requires=["click", "eth_utils", "eth-hash[pycryptodome]", "pendulum"],
+    install_requires=[
+        "click",
+        "eth_utils",
+        "eth-hash[pycryptodome]",
+        "flask",
+        "pendulum",
+    ],
     entry_points="""
     [console_scripts]
     merkle-drop=merkle_drop.cli:main
