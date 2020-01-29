640def715212515c36b1b1a06c32e51843716c98
Ralf Schmitt <ralf@brainbot.com>
2020-01-29 23:08:33 +0100

    Use setup.cfg for python package declaration
    Also provide a pyproject.toml file. Unfortunately we cannot get rid of
    setup.py yet, because pip does not support editable installs without a
    setup.py file.
    
    See https://pypi.org/project/setuptools-scm/ for more information, as
    well as PEP 517 and 518.

--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name="merkle-drop",
-    setup_requires=["setuptools_scm"],
-    use_scm_version=True,
-    packages=find_packages("src"),
-    package_dir={"": "src"},
-    install_requires=[
-        "click",
-        "web3",
-        "contract-deploy-tools",
-        "eth_utils",
-        "flask",
-        "flask_cors",
-        "pendulum",
-        "gunicorn",
-    ],
-    entry_points="""
-    [console_scripts]
-    merkle-drop=merkle_drop.cli:main
-    """,
-)
+setup(use_scm_version=True)
